553dba8beb2997f9167e4c0656db66d5cb8e5323
edilsonchaves <edilson.montenegro@gmail.com>
2021-02-23 19:58:51 -0300

    Updt Background
    Background: Updt script Buttons BackGround
    Cartas: Movimentação das Cartas;

---
+++ OperacaoLaranjaOficial/Assets/Script/Cards/CardMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CardMovement : MonoBehaviour
+{
+    [SerializeField]bool clicado;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        clicado = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            clicado = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            clicado = false;
+        }
+        if (clicado)
+        {
+            Vector3 positionCard = Camera.main.ScreenToWorldPoint(Input.mousePosition) ;
+            positionCard.z = 0;
+            transform.position = positionCard;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (clicado)
+        {
+            Debug.Log(collision.name);
+        }
+        
+    }
+}
--- OperacaoLaranjaOficial/Assets/Script/GameScript/ButtonScript.cs
+++ OperacaoLaranjaOficial/Assets/Script/GameScript/ButtonScript.cs
@@ -6,25 +6,25 @@ using UnityEngine.EventSystems;
 
 public class ButtonScript : MonoBehaviour
 {
-    public enum Button {Play,Niveis,Gravações,Configurações, Agentes_Especiais,Fase1,Fase2,Fase3,Fase4 }
+    public enum Button {Play,Niveis,Gravações,Configurações, Agentes_Especiais,Voltar_Menu,Fase1,Fase2,Fase3,Fase4 }
     public Button selectedButton;
     SpriteRenderer spriteRenderer;
     public Sprite[] status;
-
+    CameraMovement camMove;
     private void Start()
     {
         spriteRenderer=GetComponentInChildren<SpriteRenderer>();
         spriteRenderer.sprite = status[0];
+        camMove = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMovement>();
     }
     private void OnMouseDown()
     {
-        Debug.Log(this.gameObject.name);
         spriteRenderer.sprite = status[1];
     }
     private void OnMouseUp()
     {
-        Debug.Log(this.gameObject.name);
         spriteRenderer.sprite = status[0];
+        actionButton();
     }
 
 
@@ -33,22 +33,34 @@ public class ButtonScript : MonoBehaviour
         switch (selectedButton)
         {
             case Button.Play:
+                camMove.SetDestiny(2);
                 break;
             case Button.Niveis:
+                Debug.Log("Testando");
+                camMove.SetDestiny(1);
                 break;
             case Button.Gravações:
+                camMove.SetDestiny(7);
                 break;
             case Button.Configurações:
                 break;
             case Button.Agentes_Especiais:
+                camMove.SetDestiny(6);
                 break;
             case Button.Fase1:
+                camMove.SetDestiny(2);
                 break;
             case Button.Fase2:
+                camMove.SetDestiny(3);
                 break;
             case Button.Fase3:
+                camMove.SetDestiny(4);
                 break;
             case Button.Fase4:
+                camMove.SetDestiny(5);
+                break;
+            case Button.Voltar_Menu:
+                camMove.SetDestiny(0);
                 break;
         }
     }
