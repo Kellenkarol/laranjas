8cb8f5ce413aba2d9a77c253b8c34366d4461daf
JHDsBR <jesse2013.jsc@gmail.com>
2021-02-25 22:15:25 -0300

    DeckCardController
    adição de métodos

--- OperacaoLaranjaOficial/Assets/Script/GameScript/DeckCardController.cs
+++ OperacaoLaranjaOficial/Assets/Script/GameScript/DeckCardController.cs
@@ -7,20 +7,38 @@ public class DeckCardController : MonoBehaviour
 
     [Tooltip("Cartas que estarão presentes na fase")]public CardScriptable[] CardDeck;
     int randomNumberCard;
-    public GameObject CardObject;
+    public GameObject CardObject, Position;
+
+    private static List<Dictionary<Transform,bool>> allPositions = new List<Dictionary<Transform,bool>>();
+
     // Start is called before the first frame update
     void Start()
     {
+        SetAllPositions();
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)){
             sortearNovaCarta();
-
         }
     }
+
+
+    // // Test
+    // public void sortearNovaCarta()
+    // {
+    //     randomNumberCard = Random.Range(0, CardDeck.Length);
+    //     Transform pos = FindPosition(0);
+    //     GameObject cardTmp = Instantiate(CardObject) as GameObject;
+    //     cardTmp.transform.position = pos.position;
+    //     cardTmp.transform.eulerAngles = pos.eulerAngles;
+    //     SetEmptyOrNot(0, pos, false);
+    // }
+
 
     public void sortearNovaCarta()
     {
@@ -28,5 +46,46 @@ public class DeckCardController : MonoBehaviour
         Instantiate(CardObject,Vector3.zero,Quaternion.identity);
         Debug.Log(CardDeck[randomNumberCard]);
         CardObject.GetComponent<CardDisplay>().ConfigCardDisplay(CardDeck[randomNumberCard]);
+    }
+
+
+    // Encontra uma posição vazia, caso não tenha retorna null
+    public static Transform FindPosition(int nivel)
+    {
+        foreach(KeyValuePair<Transform,bool> trs in allPositions[nivel])
+        {
+            if(trs.Value)
+            {
+                return trs.Key;
+            }
+        }
+        return null;
+    }
+
+
+    // Percorre todos os gameObjects e adiciona o Transform a lista allPositions
+    private void SetAllPositions()
+    {
+        int num = Position.transform.childCount;
+        for(int c=0; c<num; c++)
+        {
+            GameObject gobTmp = Position.transform.GetChild(c).gameObject;
+            int num2 = gobTmp.transform.childCount;
+            Dictionary<Transform, bool> dictTmp = new Dictionary<Transform, bool>();
+            for(int b=0; b<num2; b++)
+            {
+                Transform trsTmp = gobTmp.transform.GetChild(b).transform;
+                dictTmp.Add(trsTmp, true);
+            }
+            allPositions.Add(dictTmp);
+        }
+    }
+
+
+    // Define se um slot está em uso ou não
+    // true = vazio
+    public static void SetEmptyOrNot(int nivel, Transform pos, bool v)
+    {
+        allPositions[nivel][pos] = v;
     }
 }
