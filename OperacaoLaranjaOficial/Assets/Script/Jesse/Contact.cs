b57e4646927d5affc700e4c8e7ccdb98487db62d
JHDsBR <jesse2013.jsc@gmail.com>
2021-02-25 23:11:59 -0300

    Linkar com outros apps
    apenas testando, nada demais.

---
+++ OperacaoLaranjaOficial/Assets/Script/Jesse/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contact : MonoBehaviour
+{
+	public void GoTo(string url)
+	{
+		Application.OpenURL(url);
+	}
+
+	public void Instagram(string username)
+	{
+		Application.OpenURL("https://www.instagram.com/"+username);
+	}
+}
