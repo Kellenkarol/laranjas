340dfd0df8273a1ca064cf49a3fc960c9f645edc
JHDsBR <Jesse2013.jsc@gmail.com>
2021-05-11 01:51:09 -0300

    Updt na lixeira

---
+++ OperacaoLaranjaOficial/Assets/LixeiraManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LixeiraManager : MonoBehaviour
+{
+	public Sprite OnOver, OnOut;
+	private SpriteRenderer mySpt;
+
+	bool enter;
+	GameObject card;
+
+
+	void Start()
+	{
+		mySpt = GetComponent<SpriteRenderer>();
+	}
+
+
+	void Update()
+	{
+		if(enter)
+		{
+    		// mySpt.sprite = OnOver;
+			if(!Input.GetMouseButton(0))
+			{
+				Destroy(card);
+			}
+		}
+		else
+		{
+    		// mySpt.sprite = OnOut;
+		}
+	}
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+    	if(col.gameObject.tag == "Card")
+    	{
+    		mySpt.sprite = OnOver;
+    		card = col.gameObject;
+    		enter = true;
+    		print("Enter");
+    	}
+    }
+
+    public void OnTriggerExit2D(Collider2D col)
+    {
+    	if(col.gameObject.tag == "Card")
+    	{
+    		mySpt.sprite = OnOut;
+    		enter = false;
+    		print("Out");
+    	}
+    }
+}
